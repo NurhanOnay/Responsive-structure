731869e56381ac4949a781dff3deec61d1d62f19
NurhanOnay <onayynurhan@gmail.com>
2025-03-05 11:55:47 +0300

    Hamburger menü eklendi

---
+++ script.js
@@ -0,0 +1,5 @@
+// Menü açma/kapama işlevi
+function toggleMenu() {
+    var menu = document.querySelector(".nav-links");  // .nav-links eklerizki menüdekiler gelsin 
+    menu.classList.toggle("show");  
+}
